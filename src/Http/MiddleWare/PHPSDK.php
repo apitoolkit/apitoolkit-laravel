c7ab4e12a69590f533fcd1314775923b55b547f9
Edinyanga O. Ottoho <elzucky@gmail.com>
2022-04-05 22:32:42 +0100

    API toolkit PHP SDK

--- src/Http/MiddleWare/PHPSDK.php
+++ src/Http/MiddleWare/PHPSDK.php
@@ -3,6 +3,8 @@
 namespace APIToolkit\SDKs;
 
 use Closure;
+use Illuminate\Support\Facades\Http;
+use Google\Cloud\PubSub\PubSubClient;
 
 //use APIToolkit\SDKS\PHPSDK;
 
@@ -14,18 +16,92 @@ class APIKeyInvalid extends Exception {
         return $message;
     }
 }
-  
+
+class TopicInvalid extends Exception {
+    public function msg() {
+        //error message
+        $message = 'Error on line '.$this->getLine().' in '.$this->getFile()
+        .': '.$this->getMessage();
+        return $message;
+    }
+}
+
+class ClientMetaDataError extends Exception {
+    public function msg() {
+        //error message
+        $message = 'Error on line '.$this->getLine().' in '.$this->getFile()
+        .': '.$this->getMessage();
+        return $message;
+    }
+}
+
 class PHPSDK
 {
-    public function handle($request, Closure $next, $APIKey=null)
+    public function handle($request, Closure $next)
     {
-        if ($APIKey == null) {
-            throw new APIKeyInvalid("API Key must be provided when associating the APIToolkit middleware to a route!");
-            return false;
+        $config = (object) [
+            "APIKey"=>env('APIToolKit_API_KEY', null),
+            "ProjectID"=>env('APIToolKit_PROJECT_ID', null),
+            "RootURL"=>env('APIToolKit_ROOT_URL', null)
+        ];
+        if ($config->APIKey == null) {
+            return new APIKeyInvalid("You haven't provided a key. Please specify a valid key 'APIToolKit_API_KEY' in your .env file");
         }
-        $this->useAPIToolkit($request, $next, $APIKey);
+        if ($config->RootURL == null) {
+            $url = "https://app.apitoolkit.io";
+        }
+        else {
+            $url = $config->RootURL;
+        }
+
+        $url = preg_replace("/\/{1}$/", "", $url);
+        
+        $APIKey = $config->APIKey;
+
+        $clientmetadata = Http::withToken($APIKey)
+            ->get($url."/api/client_metadata")->json();
+        
+        if ($clientmetadata->failed()) {
+            return new ClientMetaDataError("Unable to query APIToolkit for client metadata");
+        }
+
+        $client = new PubSubClient([
+            'projectId' => $clientmetadata["PubsubProjectId"]
+        ]);
+
+        $topic = $client->createTopic(env('APIToolKit_TOPIC_ID', "apitoolkit-go-client"));
+
+        $cl = (object) [
+            "pubsubClient"=>$client,
+		    "phpReqsTopic"=>$topic,
+		    "config"=>$config,
+		    "metadata"=>$clientmetadata
+        ];
+
+        $this->client = $cl;
+
+        $this->useAPIToolkit($request, $next, $config);
+
     }
-    public function useAPIToolkit($request, $next, $APIKey) {
+    public function publishMessage($payload) {
+        if ($this->client->phpReqsTopic == null) {
+            return new TopicInvalid("Topic is not initialized!");
+        }
+        $data = json_encode($payload);
+        $msg = $topic->publish([
+            "data" => $data,
+            "publishTime"=>time()
+        ]);
+    }
+    public function useAPIToolkit($request, $next, $config) {
+        $start = time();
+        $since = time() - $start;
+        
+        $payload = [
+
+        ];
+
+        $this->publishMessage($payload);
 
         return $next($request);
     }

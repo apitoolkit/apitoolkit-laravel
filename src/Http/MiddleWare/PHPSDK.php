59246642727df40bb95d2f2ebe7a05150ae2604a
Edinyanga O. Ottoho <elzucky@gmail.com>
2022-04-08 21:53:01 +0100

    print logs

--- src/Http/MiddleWare/PHPSDK.php
+++ src/Http/MiddleWare/PHPSDK.php
@@ -172,6 +172,8 @@ class PHPSDK
             "timestamp"=>       $timestamp,
             "url_path"=>        $path,
         ];
+
+        echo json_encode($payload, JSON_UNESCAPED_SLASHES);
         
         $this->publishMessage($payload, $request);
         

82a8211ba81652fdb21a12c6a8fab6e9a88916e3
Edinyanga O. Ottoho <elzucky@gmail.com>
2022-04-09 02:47:46 +0100

    JSON format

--- test/app/Http/Kernel.php
+++ test/app/Http/Kernel.php
@@ -62,7 +62,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        "apitoolkit" => \APIToolkit\SDKs\PHPSDK::class
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
     ];
 }
--- test/routes/web.php
+++ test/routes/web.php
@@ -6,14 +6,19 @@
 
     //Serve this on port 8000 - php artisan serve
     Route::post('/users/{user_id}/delete', function () {
-        
-    })->middleware("apitoolkit");
+        return Response::json([
+            "status"=>"success",
+            "msg"=>"User Deleted"
+        ], 200);
+    })->middleware(PHPSDK::class);
 
     //Serve this on port 8001 - php artisan serve --port 8001
     Route::get('/delete', function () {
+        
         $data = Http::withBody(json_encode(["mode"=>"admin"]), 'application/json')
             ->post("http://127.0.0.1:8000/users/123456/delete")->json();
-        echo json_encode($data);
+
+        return Response::json($data, 200);
     });
 
 ?>

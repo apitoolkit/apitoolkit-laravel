e69b32b5c966b1c0047ea5e60e066ae22c62cc27
Edinyanga O. Ottoho <elzucky@gmail.com>
2022-04-09 01:10:19 +0100

    Update web.php

--- test/routes/web.php
+++ test/routes/web.php
@@ -5,15 +5,15 @@
     use Illuminate\Support\Facades\Http;
 
     //Serve this on port 8000 - php artisan serve
-    Route::post('/users/{user_id}/delete', function () {
+    Route::get('/users/{user_id}/delete', function () {
         
     })->middleware(PHPSDK::class);
 
     //Serve this on port 8001 - php artisan serve --port 8001
     Route::get('/delete', function () {
         $data = Http::withBody(json_encode(["mode"=>"admin"]), 'application/json')
-            ->post("http://127.0.0.1:8000/users/123456/delete");
-        print_r($data);
+            ->get("http://127.0.0.1:8000/users/123456/delete")->json();
+        echo json_encode($data);
     });
 
 ?>

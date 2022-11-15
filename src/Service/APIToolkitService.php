36adb5512109d6edea14b07bdc75b5b22bd32953
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-11-15 19:41:40 +0200

    fix duration precision in sdk

--- src/Service/APIToolkitService.php
+++ src/Service/APIToolkitService.php
@@ -10,132 +10,132 @@ use \Illuminate\Http\Request;
 
 class APIToolkitService
 {
-  private static $instance;
+    private static $instance;
 
-  /**
-   * @var InvalidClientMetadataException|array
-   */
-  private $projectId;
-  private $pubsubTopic;
+    /**
+     * @var InvalidClientMetadataException|array
+     */
+    private $projectId;
+    private $pubsubTopic;
 
-  private function __construct($credentials)
-  {
-    $this->projectId = $credentials["projectId"];
-    // TODO: Is it possible to cache this pubsub client and prevent initialization on each request?
-    $pubsubClient = new PubSubClient([
-      "keyFile" => $credentials["pubsubKeyFile"]
-    ]);
-    $this->pubsubTopic = $pubsubClient->topic($credentials["topic"]);
-  }
-
-  public static function getInstance($credentials)
-  {
-    if (!self::$instance) {
-      self::$instance = new self($credentials);
+    private function __construct($credentials)
+    {
+        $this->projectId = $credentials["projectId"];
+        // TODO: Is it possible to cache this pubsub client and prevent initialization on each request?
+        $pubsubClient = new PubSubClient([
+            "keyFile" => $credentials["pubsubKeyFile"]
+        ]);
+        $this->pubsubTopic = $pubsubClient->topic($credentials["topic"]);
     }
 
-    return self::$instance;
-  }
+    public static function getInstance($credentials)
+    {
+        if (!self::$instance) {
+            self::$instance = new self($credentials);
+        }
 
-  public static function credentials($url, $api_key)
-  {
-    $url = $url . "/api/client_metadata";
-
-    $curlInit = curl_init($url);
-    curl_setopt($curlInit, CURLOPT_URL, $url);
-    curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
-
-    $headers = array(
-      "Authorization: Bearer $api_key",
-    );
-
-    curl_setopt($curlInit, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curlInit, CURLOPT_SSL_VERIFYPEER, false);
-
-    $curlResponse = curl_exec($curlInit);
-
-    $response = json_decode($curlResponse, 1);
-    if ($curlResponse == false) {
-      curl_error($curlInit);
+        return self::$instance;
     }
 
-    curl_close($curlInit);
+    public static function credentials($url, $api_key)
+    {
+        $url = $url . "/api/client_metadata";
 
-    return $response;
-  }
+        $curlInit = curl_init($url);
+        curl_setopt($curlInit, CURLOPT_URL, $url);
+        curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
 
-  public static function getCredentials()
-  {
-    $APIKey = env('APITOOLKIT_KEY');
-    $url = env("APITOOLKIT_ROOT_URL", "https://app.apitoolkit.io");
+        $headers = array(
+            "Authorization: Bearer $api_key",
+        );
 
-    $clientmetadata = self::credentials($url, $APIKey);
-    if (!$clientmetadata) {
-      return new InvalidClientMetadataException("Unable to query APIToolkit for client metadata, do you have a correct APIKey? ");
+        curl_setopt($curlInit, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curlInit, CURLOPT_SSL_VERIFYPEER, false);
+
+        $curlResponse = curl_exec($curlInit);
+
+        $response = json_decode($curlResponse, 1);
+        if ($curlResponse == false) {
+            curl_error($curlInit);
+        }
+
+        curl_close($curlInit);
+
+        return $response;
     }
 
-    return [
-      "projectId" => $clientmetadata["project_id"],
-      "pubsubKeyFile" => $clientmetadata["pubsub_push_service_account"],
-      "topic" => $clientmetadata["topic_id"]
-    ];
-  }
+    public static function getCredentials()
+    {
+        $APIKey = env('APITOOLKIT_KEY');
+        $url = env("APITOOLKIT_ROOT_URL", "https://app.apitoolkit.io");
 
-  public function publishMessage($payload)
-  {
-    $data = json_encode($payload, JSON_UNESCAPED_SLASHES);
+        $clientmetadata = self::credentials($url, $APIKey);
+        if (!$clientmetadata) {
+            return new InvalidClientMetadataException("Unable to query APIToolkit for client metadata, do you have a correct APIKey? ");
+        }
 
-    $this->pubsubTopic->publish([
-      "data" => $data
-    ]);
-  }
-
-  public function log(Request $request, $response, $startTime)
-  {
-    if (!$this->pubsubTopic) return;
-
-    $since = hrtime(true) - $startTime;
-
-    $query_params = [];
-
-    foreach ($request->all() as $k => $v) {
-      $query_params[$k] = [$v];
+        return [
+            "projectId" => $clientmetadata["project_id"],
+            "pubsubKeyFile" => $clientmetadata["pubsub_push_service_account"],
+            "topic" => $clientmetadata["topic_id"]
+        ];
     }
 
-    $request_headers = $request->header();
-    $response_headers = $response->headers;
+    public function publishMessage($payload)
+    {
+        $data = json_encode($payload, JSON_UNESCAPED_SLASHES);
 
-    $path_params = $request->route() ? $request->route()->parameters() : [];
-    $path = $request->route() ?  "/" . $request->route()->uri : $request->getRequestUri();
+        $this->pubsubTopic->publish([
+            "data" => $data
+        ]);
+    }
 
-    $timestamp = new DateTime();
-    $timestamp = $timestamp->format("c");
+    public function log(Request $request, $response, $startTime)
+    {
+        if (!$this->pubsubTopic) return;
 
-    $host = $request->getHttpHost();
+        $since = hrtime(true) - $startTime;
 
-    $referer = $request->headers->get("referer");
+        $query_params = [];
 
-    $payload = (object)[
-      "duration" => round($since * 1000),
-      "host" => $host,
-      "method" => strtoupper($request->method()),
-      "project_id" => $this->projectId,
-      "proto_major" => 1,
-      "proto_minor" => 1,
-      "query_params" => $query_params,
-      "path_params" => $path_params,
-      "raw_url" => $request->getRequestUri(),
-      "referer" => $referer ?? "",
-      "request_body" => base64_encode($request->getContent()),
-      "request_headers" => $request_headers,
-      "response_body" => base64_encode($response->getContent()),
-      "response_headers" => $response_headers->all(),
-      "sdk_type" => "PhpLaravel",
-      "status_code" => $response->getStatusCode(),
-      "timestamp" => $timestamp,
-      "url_path" => $path,
-    ];
+        foreach ($request->all() as $k => $v) {
+            $query_params[$k] = [$v];
+        }
 
-    $this->publishMessage($payload);
-  }
+        $request_headers = $request->header();
+        $response_headers = $response->headers;
+
+        $path_params = $request->route() ? $request->route()->parameters() : [];
+        $path = $request->route() ?  "/" . $request->route()->uri : $request->getRequestUri();
+
+        $timestamp = new DateTime();
+        $timestamp = $timestamp->format("c");
+
+        $host = $request->getHttpHost();
+
+        $referer = $request->headers->get("referer");
+
+        $payload = (object)[
+            "duration" => round($since),
+            "host" => $host,
+            "method" => strtoupper($request->method()),
+            "project_id" => $this->projectId,
+            "proto_major" => 1,
+            "proto_minor" => 1,
+            "query_params" => $query_params,
+            "path_params" => $path_params,
+            "raw_url" => $request->getRequestUri(),
+            "referer" => $referer ?? "",
+            "request_body" => base64_encode($request->getContent()),
+            "request_headers" => $request_headers,
+            "response_body" => base64_encode($response->getContent()),
+            "response_headers" => $response_headers->all(),
+            "sdk_type" => "PhpLaravel",
+            "status_code" => $response->getStatusCode(),
+            "timestamp" => $timestamp,
+            "url_path" => $path,
+        ];
+
+        $this->publishMessage($payload);
+    }
 }

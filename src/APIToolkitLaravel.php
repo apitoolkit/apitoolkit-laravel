decd8f5a16c7b16534e5fb11227006a6adb7519c
dawkaka <yousiph77@gmail.com>
2024-12-28 11:40:32 +0000

    let's see

--- src/APIToolkitLaravel.php
+++ src/APIToolkitLaravel.php
@@ -6,81 +6,15 @@ use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware as GuzzleMiddleware;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use APIToolkit\Common as Common;
 
-include "utils.php";
 class APIToolkitLaravel
 {
-    public static  function observeGuzzle($request, $options)
-    {
-        $handlerStack = HandlerStack::create();
-        $request_info = [];
-        $handlerStack->push(GuzzleMiddleware::mapRequest(function ($request) use (&$request_info, $options) {
-            $query = "";
-            parse_str($request->getUri()->getQuery(), $query);
-            $request_info = [
-                "start_time" => hrtime(true),
-                "method" => $request->getMethod(),
-                "raw_url" => $request->getUri()->getPath() . '?' . $request->getUri()->getQuery(),
-                "url_no_query" => $request->getUri()->getPath(),
-                "url_path" => $options['pathPattern'] ?? $request->getUri()->getPath(),
-                "headers" => $request->getHeaders(),
-                "body" => $request->getBody()->getContents(),
-                "query" => $query,
-                "host" => $request->getUri()->getHost(),
-            ];
-            return $request;
-        }));
-
-        $handlerStack->push(GuzzleMiddleware::mapResponse(function ($response) use (&$request_info, $request, $options) {
-            $apitoolkit = $request->apitoolkitData;
-            $client = $apitoolkit['client'];
-            $msg_id = $apitoolkit['msg_id'];
-            $projectId = $apitoolkit['project_id'];
-            $respBody = $response->getBody()->getContents();
-            $payload = [
-                'duration' => round(hrtime(true) - $request_info["start_time"]),
-                'host' => $request_info["host"],
-                'method' => $request_info["method"],
-                'project_id' => $projectId,
-                'proto_major' => 1,
-                'proto_minor' => 1,
-                'query_params' => $request_info["query"],
-                'path_params' => extractPathParams($request_info["url_path"], $request_info["url_no_query"]),
-                'raw_url' => $request_info["raw_url"],
-                'referer' => "",
-                'request_headers' => redactHeaderFields($options["redactHeaders"] ?? [], $request_info["headers"]),
-                'response_headers' => redactHeaderFields($options["redactHeaders"] ?? [], $response->getHeaders()),
-                'request_body' => base64_encode(redactJSONFields($options["redactRequestBody"] ?? [], $request_info["body"])),
-                'response_body' => base64_encode(redactJSONFields($options["redactResponseBody"] ?? [], $respBody)),
-                'errors' => [],
-                'sdk_type' => 'GuzzleOutgoing',
-                'parent_id' => $msg_id,
-                'status_code' => $response->getStatusCode(),
-                'timestamp' => (new \DateTime())->format('c'),
-                'url_path' => $request_info["url_path"],
-            ];
-            $client->publishMessage($payload);
-            $newBodyStream = \GuzzleHttp\Psr7\Utils::streamFor($respBody);
-
-            $newResponse = new GuzzleResponse(
-                $response->getStatusCode(),
-                $response->getHeaders(),
-                $newBodyStream,
-                $response->getProtocolVersion(),
-                $response->getReasonPhrase()
-            );
-            return $newResponse;
-        }));
-
-        $client = new Client(['handler' => $handlerStack]);
-        return $client;
+    public static  function observeGuzzle($request, $options) {
+         return Common::observeGuzzle($request, $options);
     }
-
     public static function reportError($error, $request)
     {
-        $atError = buildError($error);
-        $apitoolkit = $request->apitoolkitData;
-        $client = $apitoolkit['client'];
-        $client->addError($atError);
+        return Common::reportError($error, $request);
     }
 }
--- src/Http/Middleware/APIToolkit.php
+++ src/Http/Middleware/APIToolkit.php
@@ -2,58 +2,51 @@
 
 namespace APIToolkit\Http\Middleware;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Closure;
-use Illuminate\Http\Request;
-use Google\Cloud\PubSub\PubSubClient;
 use Exception;
-use JsonPath\JsonObject;
-use JsonPath\InvalidJsonException;
 use Ramsey\Uuid\Uuid;
-
+use APIToolkit\Common;
 
 class APIToolkit
 {
-  private string $projectId;
   public bool $debug;
-  public $pubsubTopic;
   private array $redactHeaders = [];
   private array $redactRequestBody = [];
   private array $redactResponseBody = [];
   private array $errors = [];
   private ?string $serviceVersion;
+  private ?string $serviceName;
   private array $tags = [];
 
   public function __construct()
   {
-    $apitoolkitCredentials = Cache::remember('apitoolkitInstance', 2000, function () {
-      return APIToolkit::getCredentials();
-    });
     $this->tags = array_map('trim', explode(",", env('APITOOLKIT_TAGS', "")));
     $this->serviceVersion = env('APITOOLKIT_SERVICE_VERSION', null);
+    $this->serviceName = env('APITOOLKIT_SERVICE_NAME', "");
     $this->redactHeaders = env('APITOOLKIT_REDACT_HEADERS', []);
     $this->redactRequestBody = env('APITOOLKIT_REDACT_REQUEST_BODY', []);
     $this->redactResponseBody = env('APITOOLKIT_REDACT_RESPONSE_BODY', []);
+    $this->captureRequestBody = env('APITOOLKIT_CAPTURE_REQUEST_BODY', false);
+    $this->captureResponseBody = env('APITOOLKIT_CAPTURE_RESPONSE_BODY', false);
     $this->debug = env('APITOOLKIT_DEBUG', false);
+    $this->config = [
+      'redact_headers' => $this->redactHeaders,
+      'redact_request_body' => $this->redactRequestBody,
+      'redact_response_body' => $this->redactResponseBody,
+      'serviceVersion' => $this->serviceVersion,
+      'tags' => $this->tags,
+      'capture_request_body' => $this->captureRequestBody,
+      'capture_response_body' => $this->captureResponseBody,
+      'debug' => $this->debug,
+      'serviceName' => $this->serviceName,
+    ];
 
-    $this->projectId = $apitoolkitCredentials["projectId"];
-    // TODO: Is it possible to cache this pubsub client and prevent initialization on each request?
-    $pubsubClient = new PubSubClient([
-      "keyFile" => $apitoolkitCredentials["pubsubKeyFile"]
-    ]);
-    $this->pubsubTopic = $pubsubClient->topic($apitoolkitCredentials["topic"]);
     $this->debug = env('APITOOLKIT_DEBUG', false);
     if ($this->debug) {
       Log::debug('APIToolkit: Credentials loaded from server correctly');
     }
   }
-  public function addError($error)
-  {
-    Log::debug("APIToolkit: Error added", $error);
-    $this->errors[] = $error;
-  }
-
   public function handle(Request $request, Closure $next)
   {
     $newUuid = Uuid::uuid4();
@@ -61,139 +54,43 @@ class APIToolkit
     $request = $request->merge([
       'apitoolkitData' => [
         'msg_id' => $msg_id,
-        'project_id' => $this->projectId,
-        "client" => $this,
+        'errors' => [],
       ]
     ]);
-    $startTime = hrtime(true);
     $response = $next($request);
-    $this->log($request, $response, $startTime, $msg_id);
+    $this->log($request, $response, $msg_id);
     return $response;
   }
-
-  public static function credentials($url, $api_key)
+  public function log(Request $request, $response, $msg_id, $span)
   {
-    if (empty($url)) {
-      $url = "https://app.apitoolkit.io";
-    }
-    $url = $url . "/api/client_metadata";
-
-    $headers = array(
-      "Authorization: Bearer $api_key",
-    );
-
-    $curlInit = curl_init($url);
-    curl_setopt($curlInit, CURLOPT_URL, $url);
-    curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curlInit, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curlInit, CURLOPT_SSL_VERIFYPEER, false);
-    $curlResponse = curl_exec($curlInit);
-    $response = json_decode($curlResponse, 1);
-    if ($curlResponse == false) {
-      curl_error($curlInit);
-    }
-
-    curl_close($curlInit);
-    return $response;
-  }
-
-  public static function getCredentials()
-  {
-    $APIKey = env('APITOOLKIT_KEY');
-    $url = env("APITOOLKIT_ROOT_URL", "https://app.apitoolkit.io");
-
-    $clientmetadata = self::credentials($url, $APIKey);
-    if (!$clientmetadata) {
-      throw new InvalidClientMetadataException("Unable to query APIToolkit for client metadata, do you have a correct APIKey? ");
-    }
-
-    return [
-      "projectId" => $clientmetadata["project_id"],
-      "pubsubKeyFile" => $clientmetadata["pubsub_push_service_account"],
-      "topic" => $clientmetadata["topic_id"]
-    ];
-  }
-
-  public function publishMessage($payload)
-  {
-
-    $data = json_encode($payload, JSON_UNESCAPED_SLASHES);
-    if ($this->debug) {
-      Log::debug("APIToolkit: payload" . $data);
-    }
-    $this->pubsubTopic->publish([
-      "data" => $data
-    ]);
-  }
-
-  public function log(Request $request, $response, $startTime, $msg_id)
-  {
-    if (!$this->pubsubTopic) return;
     $payload = $this->buildPayload($request, $response, $startTime, $this->projectId, $msg_id);
     if ($this->debug) {
       Log::debug("APIToolkit: payload", $payload);
     }
-    $this->publishMessage($payload);
-  }
-
-  // payload static method deterministically converts a request, response object, a start time and a projectId
-  // into a pauload json object which APIToolkit server is able to interprete.
-  public function buildPayload(Request $request, $response, $startTime, $projectId, $msg_id)
-  {
-    return [
-      'duration' => round(hrtime(true) - $startTime),
-      'host' => $request->getHttpHost(),
-      'method' => $request->getMethod(),
-      'project_id' => $projectId,
-      'proto_major' => 1,
-      'proto_minor' => 1,
-      'query_params' => $request->query->all(),
-      'path_params' =>  $request->route() ? $request->route()->parameters() : [],
-      'raw_url' => $request->getRequestUri(),
-      'referer' => $request->headers->get('referer', ''),
-      'request_headers' => $this->redactHeaderFields($this->redactHeaders, $request->headers->all()),
-      'response_headers' => $this->redactHeaderFields($this->redactHeaders, $response->headers->all()),
-      'request_body' => base64_encode($this->redactJSONFields($this->redactRequestBody, $request->getContent())),
-      'response_body' => base64_encode($this->redactJSONFields($this->redactResponseBody, $response->getContent())),
-      'sdk_type' => 'PhpLaravel',
-      'status_code' => $response->getStatusCode(),
-      'timestamp' => (new \DateTime())->format('c'),
-      'msg_id' => $msg_id,
-      'tags' => $this->tags,
-      'errors' => $this->errors,
-      'service_version' => $this->serviceVersion,
-      'url_path' => $request->route() ? $request->route()->uri : $request->getRequestUri(),
-    ];
-  }
-
-  public function redactHeaderFields(array $redactKeys, array $headerFields): array
-  {
-    array_walk($headerFields, function (&$value, $key, $redactKeys) {
-      if (in_array(strtolower($key), array_map('strtolower', $redactKeys))) {
-        $value = ['[CLIENT_REDACTED]'];
-      }
-    }, $redactKeys);
-    return $headerFields;
-  }
-
-  // redactJSONFields accepts a list of jsonpath's to redact, and a json object to redact from, 
-  // and returns the final json after the redacting has been done.
-  public function redactJSONFields(array $redactKeys, string $jsonStr): string
-  {
-    try {
-      $obj = new JsonObject($jsonStr);
-    } catch (InvalidJsonException $e) {
-      // For any data that isn't json, we simply return the data as is.
-      return $jsonStr;
-    }
-
-    foreach ($redactKeys as $jsonPath) {
-      $obj->set($jsonPath, '[CLIENT_REDACTED]');
-    }
-    return $obj->getJson();
+    $errors = $request->get('apitoolkitData')['errors'] ?? [];
+    Common.setAttributes(
+      $span,
+      $request->getHttpHost(),
+      $response->getStatusCode(),
+      $request->query->all(),
+      $request->route() ? $request->route()->parameters() : [],
+      $request->headers->all(),
+      $response->headers->all(),
+      $request->getMethod(),
+      $request->getRequestUri(),
+      $msg_id,
+      $request->route() ? $request->route()->uri : $request->getRequestUri(),
+      $request->getContent(),
+      $response->getContent(),
+      $errors,
+      $this->config,
+      'PhpLaravel',
+      null
+    );
   }
 }
 
 class InvalidClientMetadataException extends Exception
 {
 }
+
